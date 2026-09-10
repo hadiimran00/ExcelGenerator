09b4bdce0f8568f3a36cd99f26b052d661a94285
Hadi.Imran <hadiimran06@gmail.com>
2026-09-10 16:47:09 +0500

    SDMS-10626

--- src/main/java/org/example/ui/utilities/PartialReturnBudgetFlow.java
+++ src/main/java/org/example/ui/utilities/PartialReturnBudgetFlow.java
@@ -405,7 +405,7 @@ public class PartialReturnBudgetFlow {
 
             // Approve SR Level 1
             Event.robustClick(driver, By.id("forward"));
-            commentsField = wait.until(ExpectedConditions.visibilityOfElementLocated(By.id("comments")));
+            WebElement commentsField = wait.until(ExpectedConditions.visibilityOfElementLocated(By.id("comments")));
             commentsField.clear();
             commentsField.sendKeys("Approved SR Step 1");
             Event.robustClick(driver, By.id("forwardPopUpSaveBtn"));
